384ab6359bc1c7c1b8c9091280bd5476ef8f7bbe
NacaliLozano <nakaly@gmail.com>
2024-08-01 12:53:34 -0400

    Start repo

---
+++ settings.py
@@ -0,0 +1,17 @@
+# PostgreSQL
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'cZrU4x74C8q28LMcH0cYsgjS',
+        'HOST': 'postgres',
+        'PORT': '5432',
+    }
+}
+
+INSTALLED_APPS = (
+    'related_objects',
+)
+
+SECRET_KEY = 'SECRET KEY for this Django Project'
